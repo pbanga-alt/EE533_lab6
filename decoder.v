2944db67a8d7dfcdc6794fbbe4619a6fe30a4ec9
pbanga-alt <pbanga@usc.edu>
2026-02-16 12:29:28 -0800

    Add files via upload

---
+++ decoder.v
@@ -0,0 +1,159 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    16:23:46 02/13/2026 
+// Design Name: 
+// Module Name:    decoder 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+
+
+//////////////////////////////////////////////////////////////////////////////////
+module decoder( input[31:0] instr,
+					 output reg [2:0] aluctrl,
+					 output alusrc,
+					 output reg branch,
+					 output reg regwrite,
+					 output reg dmemwrite,
+					 output reg jump,
+					 output[3:0] passcond,
+					 output reg memtoreg,
+					 output trigger_debug,
+					 output stall_pipeline,
+					 output update_flags
+					 //output[15:0] immediate_num
+					 //output 
+					 
+    );
+
+/*
+Instruction format
+opcode = instr[4:0]
+cond = instr[8:5]
+register source or immediate field = instr[9]
+Source register 1 = instr[12:10]
+Source register 2 = instr[15:13]
+Destination register = instr[18:16]
+Immediate number/offset field = [27:19]
+Add/Sub offset = instr[28]
+Update flags = instr[29]
+Trigger_debug = instr[30]
+Stall = instr[31]
+*/
+
+assign alusrc = instr[9];
+assign passcond = instr[8:5];
+assign trigger_debug = instr[30];
+assign stall_pipeline = instr[31];
+assign update_flags = instr[29];
+
+always@(*)
+	begin
+		case(instr[4:0])
+			5'd1: //ldr
+					begin
+						regwrite = 1;
+						branch = 0;
+						dmemwrite = 0;
+						jump = 0;
+						memtoreg = 1;
+						aluctrl = {2'b00,instr[28]};
+						//compare_flags = 0;			
+					end
+			5'd2: //str
+					begin
+						dmemwrite = 1;
+						regwrite = 0;
+						branch = 0;
+						jump = 0;
+						memtoreg = 0;
+						aluctrl = {2'b00,instr[28]};
+						//compare_flags = 0;
+					end
+			5'd3: //add
+					begin
+					   dmemwrite = 0;
+						regwrite = 1;
+						branch = 0;
+						jump = 0;
+						memtoreg = 0;
+						aluctrl = 3'b000;
+						end
+			5'd4: //sub
+					begin
+					   dmemwrite = 0;
+						regwrite = 1;
+						branch = 0;
+						jump = 0;
+						memtoreg = 0;
+						aluctrl = 3'b001;					
+					end
+			5'd5: //lsl
+					begin
+					   dmemwrite = 0;
+						regwrite = 1;
+						jump = 0;
+						branch = 0;
+						memtoreg = 0;
+						aluctrl = 3'b010; // need to implement barrel shifter							
+					end
+			5'd6: //mov - implemented as an add I=1 immediate number = 0
+					begin
+					   dmemwrite = 0;
+						regwrite = 1;
+						branch = 0;
+						jump = 0;
+						memtoreg = 0;
+						aluctrl = 3'b000;		
+					end
+			5'd7: //cmp
+					begin
+					   dmemwrite = 0;
+						regwrite = 0;
+						jump = 0;
+						branch = 0;
+						memtoreg = 0;
+						aluctrl = 3'b001;					
+					end
+			5'd8: //branch
+					begin
+					   dmemwrite = 0;
+						regwrite = 0;
+						branch = 1;
+						jump = 0;
+						memtoreg = 0;
+						aluctrl = 3'b000;								
+					end
+			5'd9: //jump
+					begin
+					   dmemwrite = 0;
+						regwrite = 0;
+						branch = 0;
+						jump = 1;
+						memtoreg = 0;
+						aluctrl = 3'b000;								
+					end
+			default: 
+					begin
+					   dmemwrite = 0;
+						regwrite = 0;
+						branch = 0;
+						jump = 0;
+						memtoreg = 0;
+						aluctrl = 3'b000;								
+					end
+		endcase
+	end
+
+endmodule
---
+++ passdecoder.v
@@ -0,0 +1,74 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    01:42:15 02/16/2026 
+// Design Name: 
+// Module Name:    passdecoder 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module passdecoder(
+							input [3:0] compbits,
+							input N,
+							input Z,
+						   input V,
+							input C,
+							output pass
+    );
+
+reg pass_reg;
+
+parameter EQ = 4'b0000,
+			 NE = 4'b0001,
+			 CS = 4'b0010,
+			 CC = 4'b0011,
+			 MI = 4'b0100,
+			 PL = 4'b0101,
+			 VS = 4'b0110,
+			 VC = 4'b0111,
+			 HI = 4'b1000,
+			 LS = 4'b1001,
+			 GE = 4'b1010,
+			 LT = 4'b1011,
+			 GTR = 4'b1100,
+			 LE = 4'b1101,
+			 AL = 4'b1110;
+			 
+assign pass = pass_reg;
+
+always@(*)
+	begin
+		case(compbits)
+			 EQ: pass_reg = ( Z == 1);
+			 NE: pass_reg = ( Z == 0);
+			 CS: pass_reg = ( C == 1);
+			 CC: pass_reg = ( C == 0);
+			 MI: pass_reg = ( N == 1);
+			 PL: pass_reg = ( N == 0);
+			 VS: pass_reg = ( V == 1);
+			 VC: pass_reg = ( V == 0);
+			 HI: pass_reg = ( C == 1 && Z == 0);
+			 LS: pass_reg = ( C == 0 || Z == 1);
+			 GE: pass_reg = ( N == V);
+			 LT: pass_reg = ( N != V);
+			 GTR: pass_reg = ( Z == 0 && N == V);
+			 LE: pass_reg = ( Z == 1 || N != V);
+			 AL: pass_reg = 1'b1;
+			 default: pass_reg = 1'b0;
+		endcase
+	end
+
+
+
+endmodule
