cbb429152f80799af4bdc519daf55c9420b44f9a
pbanga-alt <pbanga@usc.edu>
2026-02-20 15:11:46 -0800

    Update mux8to1_64bitwide.v

--- mux8to1_64bitwide.v
+++ mux8to1_64bitwide.v
@@ -29,24 +29,23 @@ module mux8to1_64bitwide(input [63:0] I0,
 								 input S0,
 								 input S1,
 								 input S2,
-								 output [63:0] O
+								 output reg [63:0] O
     );
 	 
 wire [2:0] select;
 assign select = {S2,S1,S0};
-reg O_reg;
-assign O = O_reg;
 always@(*)
 	begin
 		case(select)
-			3'd0: O_reg = I0;
-			3'd1: O_reg = I1;
-			3'd2: O_reg = I2;
-			3'd3: O_reg = I3;
-			3'd4: O_reg = I4;
-			3'd5: O_reg = I5;
-			3'd6: O_reg = I6;
-			3'd7: O_reg = I7;
+
+			3'd0: O = I0;
+			3'd1: O = I1;
+			3'd2: O = I2;
+			3'd3: O = I3;
+			3'd4: O = I4;
+			3'd5: O = I5;
+			3'd6: O = I6;
+			3'd7: O = I7;
 		endcase
 	end
 
